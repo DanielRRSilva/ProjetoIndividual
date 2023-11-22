818d0becc871899be25cb44ad0f2c8ec57464bb2
DanielRRSilva <daniel.rsilva@sptech.school>
2023-11-21 23:15:11 -0300

    menu perfil

--- site/public/js/navBar.js
+++ site/public/js/navBar.js
@@ -1,14 +1,26 @@
 var iconBox = document.querySelector(".iconBox")
 var navBar = document.querySelector(".navBar")
+var profileIcon = document.querySelector(".profileIcon")
+var subMenu = document.querySelector(".subMenu")
+var idUser = sessionStorage.ID_USUARIO
 
 iconBox.onclick = function() {
     iconBox.classList.toggle("active")
     navBar.classList.toggle("active")
 }
 
+profileIcon.onclick = function() {
+    if(idUser != undefined) {
+        subMenu.classList.toggle("active")
+    }else {
+        alert(`Faça Login`)
+    }
+}
+
 document.onclick = function(e) {
-    if(!iconBox.contains(e.target) && !navBar.contains(e.target)) {
+    if(!iconBox.contains(e.target) && !navBar.contains(e.target) && !subMenu.contains(e.target)) {
         iconBox.classList.remove("active")
         navBar.classList.remove("active")
+        subMenu.classList.remove("active")
     }
 }
